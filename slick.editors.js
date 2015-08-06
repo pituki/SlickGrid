0286a4d2ecec2ed4f8a9a7995fecdd522fa5fe52
alex <alex.van@capco.com>
2015-08-06 17:55:09 -0400

    text editor is now extensible. defining member functions in the prototype will allow overwritting.

--- slick.editors.js
+++ slick.editors.js
@@ -21,73 +21,74 @@
   });
 
   function TextEditor(args) {
-    var $input;
-    var defaultValue;
-    var scope = this;
+    this.$input;
+    this.defaultValue;
+    this.args = args;
 
-    this.init = function () {
-      $input = $("<INPUT type=text class='editor-text' />")
-          .appendTo(args.container)
-          .bind("keydown.nav", function (e) {
-            if (e.keyCode === $.ui.keyCode.LEFT || e.keyCode === $.ui.keyCode.RIGHT) {
-              e.stopImmediatePropagation();
-            }
-          })
-          .focus()
-          .select();
-    };
-
-    this.destroy = function () {
-      $input.remove();
-    };
-
-    this.focus = function () {
-      $input.focus();
-    };
-
-    this.getValue = function () {
-      return $input.val();
-    };
-
-    this.setValue = function (val) {
-      $input.val(val);
-    };
-
-    this.loadValue = function (item) {
-      defaultValue = item[args.column.field] || "";
-      $input.val(defaultValue);
-      $input[0].defaultValue = defaultValue;
-      $input.select();
-    };
-
-    this.serializeValue = function () {
-      return $input.val();
-    };
-
-    this.applyValue = function (item, state) {
-      item[args.column.field] = state;
-    };
-
-    this.isValueChanged = function () {
-      return (!($input.val() == "" && defaultValue == null)) && ($input.val() != defaultValue);
-    };
-
-    this.validate = function () {
-      if (args.column.validator) {
-        var validationResults = args.column.validator($input.val());
-        if (!validationResults.valid) {
-          return validationResults;
-        }
-      }
-
-      return {
-        valid: true,
-        msg: null
-      };
-    };
 
     this.init();
   }
+  TextEditor.prototype.init = function () {
+    this.$input = $("<INPUT type=text class='editor-text' />")
+      .appendTo(this.args.container)
+      .bind("keydown.nav", function (e) {
+        if (e.keyCode === $.ui.keyCode.LEFT || e.keyCode === $.ui.keyCode.RIGHT) {
+          e.stopImmediatePropagation();
+        }
+      })
+      .focus()
+      .select();
+  };
+
+  TextEditor.prototype.destroy = function () {
+    this.$input.remove();
+  };
+
+  TextEditor.prototype.focus = function () {
+    this.$input.focus();
+  };
+
+  TextEditor.prototype.getValue = function () {
+    return this.$input.val();
+  };
+
+  TextEditor.prototype.setValue = function (val) {
+    this.$input.val(val);
+  };
+
+  TextEditor.prototype.loadValue = function (item) {
+    this.defaultValue = item[this.args.column.field] || "";
+    this.$input.val(this.defaultValue);
+    this.$input[0].this.defaultValue = this.defaultValue;
+    this.$input.select();
+  };
+
+  TextEditor.prototype.serializeValue = function () {
+    return this.$input.val();
+  };
+
+  TextEditor.prototype.applyValue = function (item, state) {
+    item[this.args.column.field] = state;
+  };
+
+  TextEditor.prototype.isValueChanged = function () {
+    return (!(this.$input.val() == "" && this.defaultValue == null)) && (this.$input.val() != this.defaultValue);
+  };
+
+  TextEditor.prototype.validate = function () {
+    if (this.args.column.validator) {
+      var validationResults = this.args.column.validator(this.$input.val());
+      if (!validationResults.valid) {
+        return validationResults;
+      }
+    }
+
+    return {
+      valid: true,
+      msg: null
+    };
+  };
+
 
   function IntegerEditor(args) {
     var $input;
